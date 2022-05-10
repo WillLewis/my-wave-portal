06ad8ab9b07aaea0b56810f151fc66929dc1eff5
WillLewis <willxemail@gmail.com>
2022-05-09 19:04:46 -0500

    add functions so a user can wave at us and store that wave. add a mapping of addresses to uint to store the amount of waves of any particular user. fix run.js to call functions

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,15 +1,36 @@
 // SPDX-Licens-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-
 import "hardhat/console.sol";
 
 /**
  * The contractName contract does this and that...
  */
 contract WavePortal {
+  uint256 totalWaves;
+
+  mapping (address => uint) public userWaveCount; // tracks how many times a particular user waves
+  
+
   constructor() {
   	console.log("Yo, yo I am a contract and I am smart");
     
+  }
+
+  function wave() public {
+  	totalWaves +=1;
+  	console.log("%s has waved!", msg.sender);
+  	userWaveCount[msg.sender]++;
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+  	console.log("We have %d total waves!", totalWaves);
+  	return totalWaves;
+  }
+
+  // added by me to get waves of a particular user
+  function getUserWaves() public view returns (uint256) {
+  	console.log("%s has waved %d times", msg.sender, userWaveCount[msg.sender]);
+  	return userWaveCount[msg.sender];
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,34 @@
+
+
 const main = async () => {
+	const [owner, randomPerson] = await hre.ethers.getSigners(); // get the wallet addresses
 	const waveContractFactory = await hre.ethers.getContractFactory("WavePortal"); // compiles the contract
 	const waveContract = await waveContractFactory.deploy(); // Hardhat creates a local Ethereum network just for this contract which is deleted after script completes
 	await waveContract.deployed();
+	
 	console.log("Contract deployed to:", waveContract.address); // gives address of the deployed contract
+	console.log("Contract deployed by:", owner.address); // gives address of the owner/ person deploying the contract
+
+	
+	/* 
+	* we need to manually caling our functions like we would an API
+	*/
+	let totalWaveCount; // renamed from waveCount
+	let userWaves; // waves from a particular user
+
+	totalWaveCount = await waveContract.getTotalWaves(); 
+
+	let waveTxn =  await waveContract.wave();
+	await waveTxn.wait();
+
+	totalWaveCount = await waveContract.getTotalWaves(); // get waveCount once more to see if its changed
+
+	waveTxn = await waveContract.connect(randomPerson).wave();
+	await waveTxn.wait();
+
+	totalWaveCount = await waveContract.getTotalWaves();
+
+	userWaves = await waveContract.getUserWaves();
 };
 
 const runMain = async () => {
